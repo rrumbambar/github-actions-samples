a3dca8da327e615b5d7933ccdb31dd045b3c419b
daryna.batrukh <daryna.batrukh@botscrew.com>
2024-12-05 20:39:07 +0200

    updated hello.py

--- app/hello.py
+++ app/hello.py
@@ -7,7 +7,7 @@ config.read('config.properties')
 app = Flask(__name__)
 
 if config.getboolean("features", "feature_1") == True:
-    message = "Hello, Daryna!"
+    message = "Hello, Daryna!!"
 else:
     message = "Hello, World!"
 
